909079c1c10b293744153460cc49ec555e5152c7
EvgeniiSobolev <sobolevgeny@gmail.com>
2022-10-27 21:55:28 -0400

    The project is complete

--- config/setup.js
+++ config/setup.js
@@ -1,9 +1,15 @@
 import 'dotenv/config'
 import supertest from "supertest"
+import ConfigHelper from "../helpers/config.helper";
 
 before(async function () {
    let response = await supertest(process.env.BASE_URL)
         .post('/auth')
         .send({login: process.env.LOGIN, password: process.env.PASSWORD})
     process.env['TOKEN'] = response.body.token
+})
+
+after(async function(){
+    const configHelper = new ConfigHelper()
+    await configHelper.delete()
 })
